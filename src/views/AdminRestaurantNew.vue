11f5251975923f619c9958c6cf33a7622812e30c
chuni-lin <william811566@gmail.com>
2021-03-09 20:35:36 +0800

    feat: adminRestaurantForm.vue API

--- src/views/AdminRestaurantNew.vue
+++ src/views/AdminRestaurantNew.vue
@@ -1,20 +1,41 @@
 <template>
   <div class="container py-5">
-    <AdminRestaurantForm @after-submit="handleAfterSubmit" />
+    <AdminRestaurantForm
+      :is-processing="isProcessing"
+      @after-submit="handleAfterSubmit"
+    />
   </div>
 </template>
 
 <script>
 import AdminRestaurantForm from './../components/AdminRestaurantForm.vue'
+import adminAPI from './../apis/admin'
+import { Toast } from './../utils/helpers'
 
 export default {
   components: {
     AdminRestaurantForm
   },
+  data () {
+    return {
+      isProcessing: false
+    }
+  },
   methods: {
-    handleAfterSubmit (formData) {
-      for (const [name, value] of formData.entries()) {
-        console.log(name + ': ' + value)
+    async handleAfterSubmit (formData) {
+      try {
+        this.isProcessing = true
+        const { data } = await adminAPI.restaurants.create({ formData })
+        if (data.status !== 'success') {
+          throw new Error(data.message)
+        }
+        this.$router.push({ name: 'admin-restaurants' })
+      } catch (error) {
+        this.isProcessing = false
+        Toast.fire({
+          icon: 'error',
+          title: '無法建立餐廳，請稍後再試'
+        })
       }
     }
   }
