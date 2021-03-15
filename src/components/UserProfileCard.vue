f093758d44bb5b02fa3ea954c68a91649aeb4b02
chuni-lin <william811566@gmail.com>
2021-03-15 18:38:20 +0800

    feat: user profile API

--- src/components/UserProfileCard.vue
+++ src/components/UserProfileCard.vue
@@ -3,7 +3,7 @@
     <div class="row no-gutters">
       <div class="col-md-4">
         <img
-          :src="profile.image | emptyImage"
+          :src="user.image | emptyImage"
           width="300px"
           height="300px"
         >
@@ -11,27 +11,43 @@
       <div class="col-md-8">
         <div class="card-body">
           <h5 class="card-title">
-            {{ profile.name }}
+            {{ user.name }}
           </h5>
           <p class="card-text">
-            {{ profile.email }}
+            {{ user.email }}
           </p>
           <ul class="list-unstyled list-inline">
-            <li><strong>{{ profile.commentsLength }}</strong> 已評論餐廳</li>
-            <li><strong>{{ profile.favoritedRestaurantsLength }}</strong> 收藏的餐廳</li>
-            <li><strong>{{ profile.followersLength }}</strong> followings (追蹤者)</li>
-            <li><strong>{{ profile.followingsLength }}</strong> followers (追隨者)</li>
+            <li><strong>{{ user.commentsLength }}</strong> 已評論餐廳</li>
+            <li><strong>{{ user.favoritedRestaurantsLength }}</strong> 收藏的餐廳</li>
+            <li><strong>{{ user.followersLength }}</strong> followings (追蹤者)</li>
+            <li><strong>{{ user.followingsLength }}</strong> followers (追隨者)</li>
           </ul>
-          <p>
-            <router-link :to="{name: 'user-edit', params: {id: profile.id}}">
-              <button
-                type="submit"
-                class="btn btn-primary"
-              >
-                edit
-              </button>
+          <template v-if="isCurrentUser">
+            <router-link
+              :to="{ name: 'user-edit', params: { id: user.id } }"
+              class="btn btn-primary"
+            >
+              Edit
             </router-link>
-          </p>
+          </template>
+          <template v-else>
+            <button
+              v-if="isFollowed"
+              type="button"
+              class="btn btn-danger"
+              @click.stop.prevent="deleteFollowing(user.id)"
+            >
+              取消追蹤
+            </button>
+            <button
+              v-else
+              type="button"
+              class="btn btn-primary"
+              @click.stop.prevent="addFollowing(user.id)"
+            >
+              追蹤
+            </button>
+          </template>
         </div>
       </div>
     </div>
@@ -40,12 +56,62 @@
 
 <script>
 import { emptyImageFilter } from './../utils/mixins'
+import usersAPI from './../apis/users'
+import { Toast } from './../utils/helpers'
 export default {
   mixins: [emptyImageFilter],
   props: {
-    profile: {
+    user: {
       type: Object,
       required: true
+    },
+    isCurrentUser: {
+      type: Boolean,
+      required: true
+    },
+    initialIsFollowed: {
+      type: Boolean,
+      required: true
+    }
+  },
+  data () {
+    return {
+      isFollowed: this.initialIsFollowed
+    }
+  },
+  watch: {
+    initialIsFollowed (isFollowed) {
+      this.isFollowed = isFollowed
+    }
+  },
+  methods: {
+    async addFollowing (userId) {
+      try {
+        const { data } = await usersAPI.addFollowing({ userId })
+        if (data.status === 'error') {
+          throw new Error(data.message)
+        }
+        this.isFollowed = true
+      } catch (error) {
+        Toast.fire({
+          icon: 'error',
+          title: '無法加入追蹤，請稍後再試'
+        })
+      }
+    },
+    async deleteFollowing (userId) {
+      try {
+        const { data } = await usersAPI.deleteFollowing({ userId })
+        if (data.status === 'error') {
+          throw new Error(data.message)
+        }
+        this.isFollowed = false
+      } catch (error) {
+        Toast.fire({
+          icon: 'error',
+          title: '無法取消追蹤，請稍後再試'
+        })
+      }
     }
   }
 }
